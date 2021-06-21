c0f872936bc6bd8ab55eba4e1b54d1e077ead4d8
jnyz <jnyz@163.com>
2021-06-21 17:03:42 +0800

    first commit

---
+++ src/main/java/jnyz/git/GitTest.java
@@ -0,0 +1,12 @@
+package jnyz.git;
+
+/**
+ * @Description:
+ * @Author: jnyz
+ * @date 2021/6/21 17:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("helloGit");
+    }
+}
