a537bb72cd1eb6e3ed25930f570b0409595cc8c9
jnyz <jnyz@163.com>
2021-06-21 17:06:19 +0800

    third commit

--- src/main/java/jnyz/git/GitTest.java
+++ src/main/java/jnyz/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("helloGit");
         System.out.println("helloGit2");
+        System.out.println("helloGit3");
     }
 }
