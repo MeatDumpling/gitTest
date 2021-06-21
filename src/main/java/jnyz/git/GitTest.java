eb8d93a3780e4b51893924430a84859bfce7002e
MeatDumpling <73338769+MeatDumpling@users.noreply.github.com>
2021-06-21 20:30:17 +0800

    pull commit

--- src/main/java/jnyz/git/GitTest.java
+++ src/main/java/jnyz/git/GitTest.java
@@ -15,6 +15,8 @@ public class GitTest {
         System.out.println("hot_fix :: test");
         System.out.println("hot_fix :: test");
         System.out.println("push test");
+        System.out.println("pull test");
+
 
 
     }
